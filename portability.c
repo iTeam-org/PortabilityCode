6fe0ecf6a33b66f07560e99f1023b9a40ce51724
Thomas Lange <lqp1nou@gmail.com>
2013-09-04 12:26:41 +0200

    modified:   portability.c

--- portability.c
+++ portability.c
@@ -7,8 +7,6 @@ static unsigned int _portability_color_fg = COLOR_DEFAULT;
 int
 portability_system_call(const char *cmd)
 {
-#undef system
-
    int ret = -1;
 
    if (!cmd) return -1;
@@ -32,7 +30,6 @@ portability_system_call(const char *cmd)
         // Si ce n'est pas une commande gérée, on la transmet tel quel
         ret = system(cmd);
      }
-#define system(arg) portability_system_call(arg)
    return ret;
 }
 
